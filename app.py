6bf3f91fb51f08eb080a3707cf2ffb3acc9e0571
simtbs <s.perniciaro@simt.it>
2025-08-26 10:59:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('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')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('sp.perniciaro@gmail.com')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', 'sp.perniciaro@gmail.com')
 
 mail = Mail(app)
 
