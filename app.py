3d5a83facd317d7a1c66ca56903b2aad950b4e2f
simtbs <s.perniciaro@simt.it>
2025-08-26 11:42:45 +0200

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ app = Flask(__name__)
 # Configurazione Flask-Mail
 # ----------------------
 app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.sendgrid.net')
-app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USE_SSL'] = False
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
 app.config['MAIL_USERNAME'] = 'apikey'
 app.config['MAIL_PASSWORD'] = os.environ.get('SENDGRID_API_KEY')
 app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER')
